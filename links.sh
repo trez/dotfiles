9e645ccc71b6151b0c4ec669c7397dad31d7b7cd
trez <tobias.vehkajarvi@gmail.com>
2018-08-14 11:02:39 +0200

    cmus: Use ~/.config/cmus instead of ~/.cmus

--- links.sh
+++ links.sh
@@ -7,15 +7,15 @@ mkdir -p ~/.config/nvim
 mkdir -p ~/.config/gtk-3.0
 mkdir -p ~/.config/termite
 mkdir -p ~/.xmonad
-mkdir -p ~/.cmus
+mkdir -p ~/.config/cmus
 
 # Links for dotfiles.
 ln -fsT $SCRIPT_DIR/init.vim ~/.config/nvim/init.vim
 ln -fsT $SCRIPT_DIR/gtk.css ~/.config/gtk-3.0/gtk.css
 ln -fsT $SCRIPT_DIR/xmonad.hs ~/.xmonad/xmonad.hs
 ln -fsT $SCRIPT_DIR/termite.config ~/.config/termite/config
-ln -fsT $SCRIPT_DIR/cmus.rc ~/.cmus/rc
-ln -fsT $SCRIPT_DIR/cmus.monokai ~/.cmus/monokai.theme
+ln -fsT $SCRIPT_DIR/cmus.rc ~/.config/cmus/rc
+ln -fsT $SCRIPT_DIR/cmus.monokai ~/.config/cmus/monokai.theme
 ln -fsT $SCRIPT_DIR/Xmodmap ~/.Xmodmap
 ln -fsT $SCRIPT_DIR/xinitrc ~/.xinitrc
 
